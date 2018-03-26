3d294cc4df4827f1eae9fecc59e1bf580135e79e
Miguel Sánchez de León Peque <peque@neosit.es>
2018-03-26 22:12:33 +0200

    Use the new micromouseonline mazefiles repository

--- mmsim/download.py
+++ mmsim/download.py
@@ -6,34 +6,23 @@ from urllib.request import urlretrieve
 from .typing import TarMembers
 
 
-def select_tar_members(
-        members: TarMembers, parent: Path, new_path: Path) -> TarMembers:
+def clean_tar_members(members: TarMembers) -> TarMembers:
     """
-    Select .tar members given a parent path and rename them to have a new
-    path instead.
+    Strip .tar components (i.e.: remove top-level directory) from members.
 
     Parameters
     ----------
     members
         A list of .tar members.
-    parent
-        Parent path to filter members.
-    new_path
-        New member path.
 
     Returns
     -------
-    A clean .tar member list with the selected, renamed components.
+        A clean .tar member list with the stripped components.
     """
     clean = []
     for member in members:
         path = Path(member.name)
-        if path.is_absolute():
-            raise NotImplementedError('Please, report this unexpected error!')
-        path = path.relative_to(path.parts[0])
-        if path.parent != parent:
-            continue
-        member.name = str(new_path / path.name)
+        member.name = path.relative_to(path.parts[0])
         clean.append(member)
     return clean
 
@@ -47,14 +36,10 @@ def download_micromouseonline_mazes(download_path: Path):
     download_path
         Where to download the maze files to.
     """
-    url = 'https://github.com/micromouseonline/micromouse_maze_tool/'\
+    url = 'https://github.com/micromouseonline/mazefiles/'\
         'archive/master.tar.gz'
     reveal_tar, headers = urlretrieve(url)
     with tarfile.open(reveal_tar) as tar:
-        members = select_tar_members(
-            tar.getmembers(),
-            parent=Path('mazefiles/text'),
-            new_path=Path('16x16')
-        )
+        members = clean_tar_members(tar.getmembers())
         tar.extractall(str(download_path), members)
     os.remove(reveal_tar)
