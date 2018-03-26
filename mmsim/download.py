f884503ab8b2c869659e670fbdbe943d63f526c9
Miguel Sánchez de León Peque <peque@neosit.es>
2018-03-26 22:44:14 +0200

    Fix download tests

--- mmsim/download.py
+++ mmsim/download.py
@@ -22,7 +22,39 @@ def clean_tar_members(members: TarMembers) -> TarMembers:
     clean = []
     for member in members:
         path = Path(member.name)
-        member.name = path.relative_to(path.parts[0])
+        member.name = str(path.relative_to(path.parts[0]))
+        clean.append(member)
+    return clean
+
+
+def select_tar_members(
+        members: TarMembers, parent: Path, new_path: Path) -> TarMembers:
+    """
+    Select .tar members given a parent path and rename them to have a new
+    path instead.
+
+    Parameters
+    ----------
+    members
+        A list of .tar members.
+    parent
+        Parent path to filter members.
+    new_path
+        New member path.
+
+    Returns
+    -------
+    A clean .tar member list with the selected, renamed components.
+    """
+    clean = []
+    for member in members:
+        path = Path(member.name)
+        if path.is_absolute():
+            raise NotImplementedError('Please, report this unexpected error!')
+        path = path.relative_to(path.parts[0])
+        if path.parent != parent:
+            continue
+        member.name = str(new_path / path.name)
         clean.append(member)
     return clean
 
--- mmsim/tests/test_download.py
+++ mmsim/tests/test_download.py
@@ -5,7 +5,24 @@ from tempfile import TemporaryDirectory
 import pytest
 
 from mmsim.download import download_micromouseonline_mazes
+from mmsim.download import clean_tar_members
 from mmsim.download import select_tar_members
+
+
+def test_clean_tar_members():
+    """
+    Test `clean_tar_members()` function.
+    """
+    members = [TarInfo('toplevel'),
+               TarInfo('toplevel/README.rst'),
+               TarInfo('toplevel/foo/bar0'),
+               TarInfo('toplevel/asdf/qwer/foo0.xyz')]
+    cleaned = ['.',
+               'README.rst',
+               'foo/bar0',
+               'asdf/qwer/foo0.xyz']
+    result = clean_tar_members(members)
+    assert all(x.name in cleaned for x in result)
 
 
 @pytest.mark.parametrize('parent,new_path,expected', [
@@ -33,5 +50,5 @@ def test_download_micromouseonline_mazes():
     with TemporaryDirectory() as tmpdir:
         tmpdir = Path(tmpdir)
         download_micromouseonline_mazes(tmpdir)
-        assert (tmpdir / '16x16').is_dir()
-        assert (tmpdir / '16x16' / 'japan-2011-qualifier.txt').is_file()
+        assert (tmpdir / 'classic').is_dir()
+        assert (tmpdir / 'classic' / 'apec2010.txt').is_file()
